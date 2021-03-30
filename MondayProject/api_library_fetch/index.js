a74aa11bbf43c40325f40511621a0383cfb72c91
NicholasDHarrison <50711728+NicholasDHarrison@users.noreply.github.com>
2021-03-30 17:10:13 -0400

    Add files via upload

---
+++ MondayProject/api_library_fetch/index.js
@@ -0,0 +1,16 @@
+{/* <script> */}
+function getBooks1(){
+    document.getElementById('output1').innerHTML="";
+    fetch("https://openlibrary.org/search.json?q="+document.getElementById("input1").value)
+    .then(a =>a.json())
+    .then(response=>{
+        for(var i=0; i<3; i++){
+            document.getElementById("output1").innerHTML+="<h2>"+
+            "<br>Title: &nbsp"+response.docs[i].title+"</h2>"+"By: "
+            +response.docs[i].author_name+
+            "<br><br><img src='https://covers.openlibrary.org/b/isbn/"
+            +response.docs[i].isbn[0]+"-M.jpg'><br><br><hr><br>";  }
+    });
+}
+
+
